bda40d155c93492752ba008ddfa5f03f62aa70db
mrawshan <mohamedrawshan91@gmail.com>
2025-02-09 08:48:33 +0530

    Initial commit

---
+++ app/cabins/page.js
@@ -0,0 +1,12 @@
+// Components
+import { Navigation } from '../components/Navigation';
+
+export default function Page() {
+	return (
+		<div>
+			<Navigation />
+
+			<h1>Cabins page</h1>
+		</div>
+	);
+}
