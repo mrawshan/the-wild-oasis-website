e8e99b3e691eb3eb947cda368c282ef82f7519bb
mrawshan <mohamedrawshan91@gmail.com>
2025-03-29 18:01:16 +0530

    Final mobile optimization

--- app/_components/BookingReminder.js
+++ app/_components/BookingReminder.js
@@ -13,7 +13,7 @@ function BookingReminder() {
 	if (!range.from || !range.to) return null;
 
 	return (
-		<div className='fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center'>
+		<div className='fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center max-[800px]:w-full max-[800px]:justify-center'>
 			<p>
 				<span>👋</span> Don&apos;t forget to reserve your dates <br /> from{' '}
 				{format(new Date(range.from), 'MMM dd yyyy')} to{' '}
